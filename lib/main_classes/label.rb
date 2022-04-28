597d994cc8c24a670a6cee3c2c0bbd6ced478135
gabyse1 <gaby.es.iaa@gmail.com>
2022-04-28 16:12:53 -0500

    Create the Label class and its add_item, remove_item, and to_json methods

---
+++ lib/main_classes/label.rb
@@ -0,0 +1,29 @@
+class Label
+  attr_accessor :title, :color
+  attr_reader :id, :items
+
+  def initialize(title, color, id: Random.rand(1..1000))
+    @id = id
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.label = self
+  end
+
+  def remove_item(item)
+    @items = @items.reject { |e| e.class.name == item.class.name && e.id == item.id }
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => @id,
+      'title' => @title,
+      'color' => @color
+    }.to_json(*args)
+  end
+end
